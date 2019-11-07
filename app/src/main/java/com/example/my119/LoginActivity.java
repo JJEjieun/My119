2409bfe8ef12fd6084d0bd2ea2938aa82878972d
dnri2 <20171989@sungshin.ac.kr>
2019-11-07 21:42:33 +0900

    회원가입창/메인창 employer, employee 둘다 대충 만들어놓음. 인증번호 받고 확인하는 페이지 앞으로 만들어야함.

--- app/src/main/java/com/example/my119/LoginActivity.java
+++ app/src/main/java/com/example/my119/LoginActivity.java
@@ -8,5 +8,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+//        처리필요
+//        개인회원 기업회원 클릭시에 회원가입창 다르게 뜨게
     }
 }
