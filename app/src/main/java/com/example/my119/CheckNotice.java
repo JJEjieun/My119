7e135e7436bb8521fec7cc6e1207780ff8ad2e18
dnri2 <20171989@sungshin.ac.kr>
2019-11-12 13:53:12 +0900

    로그인, 회원가입창 수정

---
+++ app/src/main/java/com/example/my119/CheckNotice.java
@@ -0,0 +1,13 @@
+package com.example.my119;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckNotice extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_notice);
+    }
+
+}
