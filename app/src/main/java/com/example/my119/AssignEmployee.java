5495de48b80e83fd449cf18dc9de37445abc6e47
mandydayfly <20171998@sungshin.ac.kr>
2019-11-14 00:04:44 +0900

    aa

--- app/src/main/java/com/example/my119/AssignEmployee.java
+++ app/src/main/java/com/example/my119/AssignEmployee.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class AssignEmployee extends AppCompatActivity {
+
+    private String verificationId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,12 @@ public class AssignEmployee extends AppCompatActivity {
 //        3) 전화번호 형식에 맞게 입력했는지
 //        4) 생년월일 맞게 입력했는지
 //        5) 인증번호 맞아야 등록처리되게
+        EditText enterPhoneNumber = findViewById(R.id.enterPhoneNumber);
+        String phoneNumber = "+82" + enterPhoneNumber.getText().toString();
+
+        sendVerificationCode(phoneNumber);
+
+        findViewById(R.id.)
 
         // 위의 항목들이 모두 정상적으로 처리 되었으면
         // '등록' 버튼 누르면 로그인창으로 돌아감
@@ -31,6 +47,40 @@ public class AssignEmployee extends AppCompatActivity {
                 }
             });
         }
+
+
     }
+
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);
+
+        );
+
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(AssignEmployee.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
 
 }
