1204acc3f862a9139e9aa5a587697ab8bdd82fe1
dnri2 <20171989@sungshin.ac.kr>
2019-11-13 23:18:10 +0900

    개인회원가입에 인증번호 수정. 이따가 이미지등록할때 입력할 버튼도 넣을 예정임.

--- app/src/main/java/com/example/my119/AssignEmployee.java
+++ app/src/main/java/com/example/my119/AssignEmployee.java
@@ -5,13 +5,47 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class AssignEmployee extends AppCompatActivity {
+    boolean isVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assign_employee);
+
+        //인증번호 받기 버튼누르면 인증번호 확인 표시 뜨게
+        Button checkPhoneNumber = (Button)findViewById(R.id.checkPhoneNumber);
+        final TextView textAut = (TextView)findViewById(R.id.textAut);
+        final EditText EnterAutNum = (EditText)findViewById(R.id.enterAutNumber);
+        final Button butAutNum = (Button)findViewById(R.id.checkAutNumber);
+
+        //인증번호가 맞으면. 나중에 if문 조건에 true부분을 수정해야함.
+        if(true)
+        checkPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(!isVisible){
+                    textAut.setVisibility(View.VISIBLE);
+                    EnterAutNum.setVisibility(View.VISIBLE);
+                    butAutNum.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        //인증번호 확인
+        butAutNum.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //인증번호 맞은 거 처리하기-if(true)부분 수정
+                if(true) {
+                    Toast.makeText(getApplicationContext(), "인증번호가 확인되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 //        처리 필요.
 //        1) 모든항목입력해야 인증번호 받을 수 있게
