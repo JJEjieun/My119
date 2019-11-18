5e35b62d01dc51ec45f2589513521ee69cdf4c74
dnri2 <20171989@sungshin.ac.kr>
2019-11-19 02:27:31 +0900

    개인회원가입 성별 라디오버튼 수정 / 마이페이지 연결

--- app/src/main/java/com/example/my119/MainEmployee.java
+++ app/src/main/java/com/example/my119/MainEmployee.java
@@ -14,6 +14,16 @@ public class MainEmployee extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_employee);
 
+        //마이페이지 버튼 누르면 마이페이지 창으로 넘어감
+        Button btnMyPage = (Button)findViewById(R.id.MyPage);
+        btnMyPage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "마이페이지", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MyPage.class);
+                startActivity(intent);
+            }
+        });
+
         // '공고 더 확인하기' 버튼 누르면 공고 확인 창으로 넘어감
         Button btnMoreNotice = (Button)findViewById(R.id.button2);
         btnMoreNotice.setOnClickListener(new View.OnClickListener() {
