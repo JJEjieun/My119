570762c5123a283ac13f37175db0352f6cf8de65
dnri2 <20171989@sungshin.ac.kr>
2019-11-20 22:33:30 +0900

    notice 약간 수정

---
+++ app/src/main/java/com/example/my119/Notice.java
@@ -0,0 +1,42 @@
+package com.example.my119;
+
+public class Notice {
+    private String noticeName;
+    private String endTime;
+    private String Store;
+    private String money;
+
+    public Notice() { }
+
+    public String getNoticeName() {
+        return noticeName;
+    }
+
+    public void setNoticeName(String noticeName) {
+        this.noticeName = noticeName;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStore() {
+        return Store;
+    }
+
+    public void setStore(String store) {
+        Store = store;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+}
