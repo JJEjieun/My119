a217127ec33da8de61aec3860b576c743129a215
jhjoo <jhjoo0303@naver.com>
2019-11-24 17:29:13 +0900

    ! 이력서 사진

--- app/src/main/java/com/example/my119/MyPage.java
+++ app/src/main/java/com/example/my119/MyPage.java
@@ -1,7 +1,11 @@
 package com.example.my119;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MyPage extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MyPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+
+        Button button_resume = (Button)findViewById(R.id.manageS);
+        button_resume.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "이력서 작성 및 변경", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Resume.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/my119/Resume.java
+++ app/src/main/java/com/example/my119/Resume.java
@@ -1,14 +1,41 @@
 package com.example.my119;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Resume extends AppCompatActivity {
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resume);
+
+        imageview = (ImageView)findViewById(R.id.myFace);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+
+        }
+
+    }
 }
