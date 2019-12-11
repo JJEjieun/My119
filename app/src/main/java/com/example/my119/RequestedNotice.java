9669a755cfb6ba65fc887eb644371b02d9b003ee
mandydayfly <20171998@sungshin.ac.kr>
2019-12-12 01:02:30 +0900

    call&videocall

--- app/src/main/java/com/example/my119/RequestedNotice.java
+++ app/src/main/java/com/example/my119/RequestedNotice.java
@@ -1,16 +1,94 @@
 package com.example.my119;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class RequestedNotice extends AppCompatActivity  {
+
+    Button call;
+    Button facecall;
+    String tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requested_notice);
 
+        call = findViewById(R.id.btn_call);
+        facecall = findViewById(R.id.btn_face_call);
 
+//        Intent callIntent = new Intent()
+
+        tel = "01023275437";
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCall();
+            }
+        });
+
+        facecall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onVideoCall();
+            }
+        });
+    }
+
+    public void onCall() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    Integer.parseInt("123"));
+        } else {
+            startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:"+tel)));
+        }
+    }
+
+    public void onVideoCall() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    Integer.parseInt("123"));
+        } else {
+            startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:"+tel)).putExtra("videocall", true));
+            }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case 123:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    onCall();
+                } else {
+                    Log.d("TAG", "Call Permission Not Granted");
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
 }
