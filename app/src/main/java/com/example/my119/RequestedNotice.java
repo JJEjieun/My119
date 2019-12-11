c568b80ee3e619793bf057e14ab4210f8fd718af
dnri2 <20171989@sungshin.ac.kr>
2019-12-11 20:56:14 +0900

    zzzz

---
+++ app/src/main/java/com/example/my119/RequestedNotice.java
@@ -0,0 +1,16 @@
+package com.example.my119;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RequestedNotice extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.requested_notice);
+
+
+
+    }
+}
